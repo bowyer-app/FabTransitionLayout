d23d09e85ad3f2d0ea3609d5ed7ebbd7efbb93c5
bowyer-app <bowyer.app@gmail.com>
2015-08-04 22:19:44 +0900

    initial commit

---
+++ fabtransitionlayout/src/main/java/com/bowyer/app/fabtransitionlayout/ViewUtils.java
@@ -0,0 +1,43 @@
+package com.bowyer.app.fabtransitionlayout;
+
+import android.support.v4.view.ViewCompat;
+import android.view.View;
+
+/**
+ * Created by Bowyer on 2015/07/15.
+ */
+public class ViewUtils {
+
+    public static boolean setVisibility(View v, boolean visible) {
+        int visibility;
+        if (visible) {
+            visibility = View.VISIBLE;
+        } else {
+            visibility = View.GONE;
+        }
+        v.setVisibility(visibility);
+        return visible;
+    }
+
+    public static boolean setInVisibility(View v, boolean visible) {
+        int visibility;
+        if (visible) {
+            visibility = View.VISIBLE;
+        } else {
+            visibility = View.INVISIBLE;
+        }
+        v.setVisibility(visibility);
+        return visible;
+    }
+
+    public static float centerX(View view) {
+        return ViewCompat.getX(view) + view.getWidth() / 2f;
+    }
+
+    public static float centerY(View view) {
+        return ViewCompat.getY(view) + view.getHeight() / 2f;
+    }
+
+    private ViewUtils() {
+    }
+}
